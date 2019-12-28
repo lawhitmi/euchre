a29a9372460922fbd6819aeffc86c8761e937957
Lucas Whitmire <lucas.whitmire@gmail.com>
2019-12-28 20:46:56 +0100

    Updated source code convention section of README

--- tests/test_euchre.py
+++ tests/test_euchre.py
@@ -1,4 +1,5 @@
 from src.euchre.euchre import pick_dealer, game, check_for_winner
+from src.euchre.euchre import ComputerHand, UserHand
 
 
 def test_pick_dealer():
@@ -8,11 +9,26 @@ def test_pick_dealer():
 
 
 def test_check_for_winner():
-    NotImplemented
+    user = UserHand(makerflag=True)
+    computer = ComputerHand(makerflag=False)
+
+    # No Winner
+    score = {user: 1, computer: 2}
+    assert check_for_winner(score) is None
+    # Maker wins with all 5
+    score = {user: 5, computer: 0}
+    assert check_for_winner(score) == tuple((user, 2))
+    # Maker wins with less than 5
+    score = {user: 4, computer: 1}
+    assert check_for_winner(score) == tuple((user, 1))
+    # Maker is euchred
+    score = {user: 1, computer: 3}
+    assert check_for_winner(score) == tuple((computer, 2))
+    # End of round is reached
+    score = {user: 2, computer: 3}
+    assert check_for_winner(score) == tuple((computer, 2))
 
 
 def test_game():
     # game()
     NotImplemented
-
-
