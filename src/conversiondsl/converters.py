8c1106841e7992125c3b642ff24eb8b77bcd39e1
Lucas Whitmire <lucas.whitmire@gmail.com>
2020-01-07 14:59:08 +0100

    Added multiple docstrings

--- src/conversiondsl/converters.py
+++ src/conversiondsl/converters.py
@@ -3,25 +3,46 @@ from functools import reduce
 
 
 def convert(imperial_units):
+    #TODO make this dynamic in the future to work for other units
     return str(round(imperial_units * 0.3148, 2)) + ' m'
 
 
 def build_structure(inputs):
+    """
+    Takes in inputs and builds FeetAndInches structure
+    :param inputs:
+    :return:
+    """
     feet, inches, fractional_inches = inputs
     feet_structure = FeetAndInches(feet, inches, fractional_inches)
     return feet_structure
 
 
 def get_decimal(instance):
+    """
+    Wrapper which just calls the .getDecimal method of the passed instance of unitsclass
+    :param instance: type unitsclasses
+    :return: float
+    """
     return instance.getDecimal()
 
 
 def add(instances):
+    """
+    Adds two measurement instances
+    :param instances: tuple of instances from the unitsclasses module
+    :return: new instance of same unitsclass class
+    """
     instance1, instance2 = instances
     return instance1 + instance2
 
 
 def build_tuple_for_feet_structure(quantity):
+    """
+    Builds the tuple required to create a FeetAndInches object
+    :param quantity: string containing the feet, inches, and fractional inches
+    :return: tuple containing feet, inches, and calculated fractional inches
+    """
     feet = float(quantity[0])
     inches = float(quantity[1])
     fractional_inches = quantity[2].split('/')
@@ -48,6 +69,11 @@ def parser(line):
 
 
 def return_results(results_map):
+    """
+    This function has side effects.  Prints to std out.
+    :param results_map: map object
+    :return: None
+    """
     result_strings = tuple(results_map)
     for i in result_strings:
         print(i)
